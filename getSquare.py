4d8656e77ff588b541cc94ec821e2a5802defdae
Darya Verzhbinsky <darya99@gmail.com>
2018-07-09 09:00:24 -0700

    updated tabbing?

--- getSquare.py
+++ getSquare.py
@@ -4,21 +4,21 @@ import math
 # Return an Image loaded from the specified file.
 #  * filename: string, name of file to load
 def load_img(filename):
-  im = Image.open(filename)
-  return im
+    im = Image.open(filename)
+    return im
 
 # Display Image to the user (for debugging purposes).
 #   * im: Image to display
 def show_img(im):
-  im.show()
+    im.show()
 
 # Save the Image to a file with the specified filename,
 #  then show the Image to the user.
 #  * im: Image to be saved
 #  * filename: string, name to save file as
 def save_img(im, filename):
-  im.save(filename, "jpeg")
-  show_img(im)
+    im.save(filename, "jpeg")
+    show_img(im)
 
 # Return a new Image, with Obamicon filter applied.
 #  * im: Image to be filtered
